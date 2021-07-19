055127d44958e53f5d8d504f8658351de0243b92
João Pedro Alves <jotalmeida007@hotmail.com>
2021-07-19 11:20:00 -0300

    Add json response lib

--- mix.exs
+++ mix.exs
@@ -26,9 +26,8 @@ defmodule GenReport.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:credo, "~> 1.5", only: [:dev, :test], runtime: false}
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
+      {:json_response, git: "https://github.com/joaopealves/json_response"}
     ]
   end
 end
